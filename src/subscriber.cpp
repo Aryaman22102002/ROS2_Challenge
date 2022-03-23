1e57376f1c9d3a4aaf93deac594a9f3f199b29c7
Aryaman <aryashardul2002@gmail.com>
2022-03-23 11:38:39 +0530

    Added the publisher and susbcriber codes

---
+++ src/subscriber.cpp
@@ -0,0 +1,35 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+// Creating the minimum subscriber class
+class MinimalSubscriber : public rclcpp::Node
+{
+  // Initializing the subscriber node
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  // Callback function for the subscriber node
+  private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      // Printing the data received by the subscriber
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
